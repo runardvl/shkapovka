37c9970b9db5b6c1f937a51dce4f94b8f2b83b29
Runar Shakirov <runardvl@gmail.com>
2021-02-25 00:31:15 +0300

    dev rules page added

--- pages/rules.vue
+++ pages/rules.vue
@@ -1,11 +1,29 @@
 <template>
-  <div class="container">
-    <h2>Правила</h2>
+  <div class="rules-page">
+    <div class="container">
+      <h1 class="rules-page__heading">Правила и полезная инфа</h1>
+    </div>
+    <rulesSection />
   </div>
 </template>
 
 <script>
+import rulesSection from '@/components/sections/rulesSection'
+
 export default {
   name: 'Rules',
+  components: {
+    rulesSection,
+  },
 }
 </script>
+
+<style lang="scss">
+.rules-page {
+  padding-top: 64px;
+  &__heading {
+    color: $darkText;
+    @include heading1;
+  }
+}
+</style>
